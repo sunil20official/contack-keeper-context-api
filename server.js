b4c53093b7fae87e79edec0052bf878e5aefb9c5
usersunilkumar <imsunilkumar20@gmail.com>
2021-08-31 23:47:38 +0530

    'deploying'

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 
 const app = express();
-app.use(cors())
+app.use(cors());
 
 // Connect Database
 connectDB();
@@ -25,9 +25,10 @@ if(process.env.NODE_ENV === 'production') {
     //set static folder
     app.use(express.static('client/build'));
 
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname,'client','build','index.html')));
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    );
 }
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
